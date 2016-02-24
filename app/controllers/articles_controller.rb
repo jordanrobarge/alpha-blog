4d92455f61bd26a5796f68a53613e848d52657e5
jordanrobarge <jordanrobarge@gmail.com>
2016-02-24 22:24:45 +0000

    Update authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     #can user debugger to stop server while running
     #render plain: params[:artcle].inspect
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was created"
       redirect_to article_path(@article)
